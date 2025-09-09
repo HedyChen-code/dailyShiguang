35da186899601193caa40471a1f553be2f7c73b7
hoganalin <hoganalin@gmail.com>
2025-09-09 17:20:41 +0800

    首頁/footer/header

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log('Hello world');
+new Swiper('.swiper', {
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  spaceBetween: 16,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  slidesPerView: 4.5,
+  breakpoints: {
+    576: { // >576px (手機)
+      slidesPerView: 6.5,
+    },
+  },
+});
+// 主視覺js
+const swiper = new Swiper(".mainSwiper", {
+  loop: true, // ✅ 加這行才能前後循環
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // ✅ 點 pagination 可以切換
+  },
+});
